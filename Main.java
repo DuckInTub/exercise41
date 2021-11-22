ca8c26149ac79108e08f0d8b02e932c3a30089f6
DuckInTub <61461064+DuckInTub@users.noreply.github.com>
2021-11-22 15:50:47 +0100

    Implemented Tuple
    Implemented the Tuple class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Tuple<Integer, Integer> t = new Tuple<>(5, 6);
+        System.out.println(t.getFirst());
+        System.out.println(t.getSecond());
+    }
+}
--- Tuple.java
+++ Tuple.java
@@ -1,7 +1,21 @@
 /**
  * Created by Niklas on 2016-02-07.
  */
-public class Tuple {
-    // public ... fst(){...}
-    // public ... snd(){...}
+public class Tuple<Type1, Type2>{
+    private final Type1 first;
+    private final Type2 second;
+
+    public Tuple(Type1 first, Type2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Type1 getFirst() {
+        return first;
+    }
+
+    public Type2 getSecond() {
+        return second;
+    }
 }
+
